181c49e1dcdfca1cac0b3f9c37e137107972b957
celldra <qu3oa6wh@anonaddy.me>
2023-02-06 12:17:29 +0000

    remove snappy compression

--- NetCache/DefaultSerializer.cs
+++ NetCache/DefaultSerializer.cs
@@ -1,28 +1,23 @@
-using System.Text;
 using System.Text.Json;
-using IronSnappy;
 using NetCache.Interfaces;
 
 namespace NetCache
 {
     /// <summary>
-    ///     The default serializer for netCache implements Snappy compressed System.Text.Json
+    ///     The default serializer for netCache implements System.Text.Json
     /// </summary>
     public class DefaultSerializer : ISerializer
     {
         public ReadOnlySpan<char> Serialize<TObj>(TObj obj)
         {
-            var json = JsonSerializer.SerializeToUtf8Bytes(obj).AsSpan();
-            var compressed = Snappy.Encode(json).AsSpan();
-            return Encoding.UTF8.GetString(compressed).AsSpan();
+            var json = JsonSerializer.Serialize(obj).AsSpan();
+            return json;
         }
 
         public TObj? Deserialize<TObj>(ReadOnlySpan<char> rawData)
         {
-            if (rawData.IsEmpty) return default; // Disallow empty input
-            
-            var uncompressed = Snappy.Decode(Encoding.UTF8.GetBytes(rawData.ToString()).AsSpan());
-            return JsonSerializer.Deserialize<TObj>(uncompressed);
+            return rawData.IsEmpty ? default : // Disallow empty input
+                JsonSerializer.Deserialize<TObj>(rawData);
         }
     }
 }
